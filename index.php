ea0123807829fce12a95e03b7203eab101edca00
Flavio <flavio.95fhs@gmail.com>
2021-08-08 15:34:06 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -3,15 +3,26 @@
 require_once("config.php");
 
 //$sql = new Sql();
-
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 //echo json_encode($usuarios);
 
+
+//Carrega um usuário
+//$usuario = new Usuario();
+//$usuario->loadById(3);
+//echo $usuario;
+
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario usando o login e a senha
 $usuario = new Usuario();
-
-$usuario->loadById(3);
-
+$usuario->login('root', '45612');
 echo $usuario;
 
  ?>
